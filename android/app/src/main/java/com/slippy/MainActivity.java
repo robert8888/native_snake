2ba34249235c3a00fff59ad88a5b268f540dc467
robert_kam <robert.kaminski@websolutionsnyc.com>
2025-04-17 01:12:49 +0200

    Migrate to latest gradle dependencies

---
+++ android/app/src/main/java/com/slippy/MainActivity.java
@@ -0,0 +1,10 @@
+package com.slippy;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+    @Override
+    protected String getMainComponentName() {
+        return "NativeSnake";
+    }
+}
